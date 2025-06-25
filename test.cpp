ef8dbbd9feda96eea7dfc9ba8a73800d2e09ba44
Sohail <sohailraj.satapathy@gmail.com>
2025-06-25 10:45:41 +0530

    test

--- test.cpp
+++ test.cpp
@@ -56,4 +56,8 @@ int main() {
 
     std::cout << "Size of double and long double: " << sizeof(double) << " " << sizeof(long double)
               << std::endl;
+    std::cout << (1.0 / 0.0f) << std::endl; // can divide float by 0 to get inf
+                                            // compiler wont raise warning if divided by 0.0f
+                                            // will raise warning if divided by 0
+                                            // will raise error if first number is an integer (1)
 }
