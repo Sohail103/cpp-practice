6979c3cfda930816182dadb0655091bf4c2d410c
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-04 16:47:34 +0530

    memory leaks

--- test.cpp
+++ test.cpp
@@ -171,4 +171,9 @@ int main() {
     {
         std::cerr << e.what() << '\n';
     }
+    {
+        // int *p_number1{new int{15}}; // leads to memory leak because variable p_number1 dies out
+        //  after its local scope is exited - memory leaks in nested scopes
+    }
+    return 0;
 }
