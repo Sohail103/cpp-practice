c6c6fec778c7b475ad3953d04b42c2f080290e7f
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-07 12:16:10 +0530

    references

--- test.cpp
+++ test.cpp
@@ -208,5 +208,16 @@ int main() {
     std::cout << "\n";
     */
 
-        return 0;
+    int int_value{45};
+    double double_value{33.65};
+    int &reference_to_int_value1{int_value};
+    // int &reference_to_int_value2{int_value};
+    // double &reference_to_double_value{double_value};
+
+    std::cout << "int value: " << int_value
+              << " and reference to int value: " << reference_to_int_value1 << std::endl;
+    std::cout << "address of int value: " << &int_value
+              << " and address of reference: " << &reference_to_int_value1 << std::endl;
+
+    return 0;
 }
