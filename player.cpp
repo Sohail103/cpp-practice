8a9f9ec9b03237da10f0b390a59cd6dd6253bb44
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-16 10:39:53 +0530

    constructor, destructor and getter definitions

--- player.cpp
+++ player.cpp
@@ -1,1 +1,9 @@
-#include ""
+#include "player.h"
+
+Player::Player() = default;
+
+Player::Player(int gamenum) { this->gamenum = gamenum; }
+
+Player::~Player() = default;
+
+int Player::getgamenum() { return gamenum; }
