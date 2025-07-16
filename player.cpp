575e6c0e303987c270dbfab663290a1cbdf5eec4
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-16 10:49:10 +0530

    fixed constructor

--- player.cpp
+++ player.cpp
@@ -2,7 +2,11 @@
 
 Player::Player() = default;
 
-Player::Player(int gamenum) { this->gamenum = gamenum; }
+Player::Player(std::string_view name, int age, int gamenum) {
+    this->name = name;
+    this->age = new int{age};
+    this->gamenum = gamenum;
+}
 
 Player::~Player() = default;
 
@@ -11,4 +15,5 @@ int Player::getgamenum() { return gamenum; }
 std::ostream &operator<<(std::ostream &out, Player &p) {
     out << "Name: " << p.getname() << ", Age: " << *(p.getage())
         << ", Game number: " << p.getgamenum() << "\n";
+    return out;
 }
