afe4f38738dbbe9d28da6852508e8b1d58d45b14
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-16 10:46:25 +0530

    fixed include guards

--- class-test.cpp
+++ class-test.cpp
@@ -1,7 +1,9 @@
-#include "person.h"
+#include "player.h"
 
 int main() {
     Person p1("abcd", 19);
+    Player player(25);
     std::cout << p1 << std::endl;
+    std::cout << player << std::endl;
     return 0;
 }
