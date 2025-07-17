db4039c5b8ab00f6e545b895c1a85c67029d5066
Sohail103 <sohailraj.satapathy@gmail.com>
2025-07-17 11:26:09 +0530

    copy constructor

--- class-test.cpp
+++ class-test.cpp
@@ -3,7 +3,9 @@
 int main() {
     Person p1("abcd", 19);
     Player player("efgh", 50, 25);
+    Player playercopy(player);
     std::cout << p1 << std::endl;
     std::cout << player << std::endl;
+    std::cout << playercopy << std::endl;
     return 0;
 }
